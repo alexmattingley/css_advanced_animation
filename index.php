c87dae5079874ad34aba0fbadb2e89a7a2826476
Alex Mattingley <alexmattingley@gmail.com>
2015-08-20 08:04:00 -0700

    got the random color generator to work, im getting the random_color to feed into the circle ocrrectly

--- index.php
+++ index.php
@@ -4,16 +4,19 @@
 	<meta charset="UTF-8">
 	<title>CSS advanced animations</title>
 	<style>
-		div {
+		.line {
 			margin: 50vw 20vw;
 			height: 1px;
 			width: 50vw;
+			position: relative;
 			background-color: red;
-			animation-name: spin;
+			animation-name: spin_line;
 			animation-duration: 4s;
+			/*animation-iteration-count: infinite;*/
 			
 		}
-		@keyframes spin {
+
+		@keyframes spin_line {
 			from {
 				transform:rotateZ(0deg);
 				-webkit-transform: rotateZ(0deg);
@@ -24,9 +27,47 @@
 			}
 		}
 
+		.circle {
+			height: 15px;
+			width: 15px;
+			border-radius: 100%;
+			position: absolute;
+			animation-name: circle_move;
+			animation-duration: 3s;
+			/*animation-iteration-count: infinite;*/
+		}
+
+		@keyframes circle_move{
+			0% {
+				left: 0;
+			}
+			50% {
+				left: 100%;
+			}
+			100% {
+				left:0;
+			}
+		}
+		
+
 	</style>
 </head>
 <body>
-	<div></div>
+	<button type="button">Random colors!</button>
+	<div class="line">
+		<div class="circle"></div>
+	</div>
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script>
+		$('button').click(function(){
+			var r = Math.round(Math.random()*(255 - 0));
+			var b = Math.round(Math.random()*(255 - 0));
+			var g = Math.round(Math.random()*(255 - 0));
+			//console.log("rgb(",r,",",b,",",g,")");
+			var random_color = "rgb(" + r + "," + b + "," + g + ")";
+			console.log(random_color);
+			$('.circle').css('border', 'solid 1px ' + random_color); 
+		});
+	</script>
 </body>
 </html>
